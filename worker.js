f6c85e545f631e763f413bcb6242d4adbb29b7b6
michaellarick-cmd <michael.larick@gmail.com>
2026-08-13 10:31:35 -0400

    Load Family calculation correction

--- worker.js
+++ worker.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const response = await env.ASSETS.fetch(request);
+    const type = response.headers.get("content-type") || "";
+    if (type.includes("text/html")) {
+      const html = await response.text();
+      const injected = html.replace("</body>", '<script src="/family-fix.js"></script></body>');
+      return new Response(injected, { status: response.status, headers: response.headers });
+    }
+    return response;
   },
 };
