ada6b1b235026e601cc0d35a7cf4cce0f27fe7d3
michaellarick-cmd <michael.larick@gmail.com>
2026-09-12 20:23:33 -0400

    Bust Safari mobile-fix cache

--- public/buffet-ui.js
+++ public/buffet-ui.js
@@ -11,7 +11,7 @@
   };
 
   const mobileFixes=document.createElement('script');
-  mobileFixes.src='/buffet-mobile-fixes.js?v=4';
+  mobileFixes.src='/buffet-mobile-fixes.js?v=5';
   mobileFixes.onload=loadAllocation;
   mobileFixes.onerror=loadAllocation;
   document.head.appendChild(mobileFixes);
